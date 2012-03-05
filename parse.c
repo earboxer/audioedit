a907d356ecefdd7449ac0beff99a2afb5dea5ffe
Meitian Huang <kid85783388@gmail.com>
2012-03-05 18:20:26 +1100

    Memcpy

---
+++ parse.c
@@ -0,0 +1,61 @@
+/*-
+ * Copyright (c) 2012 Meitian Huang
+ * All rights reserverd.
+ *
+ * Distributed under FreeBSD License.
+ */
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "dbg.h"
+
+char *in = NULL;
+char *out = NULL;
+int bflag = 0;
+int bSamples = 0;
+int eflag = 0;
+int eSamples = 0;
+int c;
+
+void print_help(){
+    printf("OPTIONS\n");
+    printf("\t-help\t\tdisplay the command line options\n");
+    printf("\t-version\tdisplay the version number\n");
+    printf("\t-tb n\t\ttrim n samples from the beginning for the audio clip\n");
+    printf("\t-te m\t\ttrim m samples off the end of the audio clip\n");
+    printf("\t-i file\t\tprovide the input file name\n");
+    printf("\t-o file\t\tprovide the output file name(overwriting an existing file)\n");
+}
+
+void parse(int argc, char **argv)
+{
+    while ((c = getopt(argc, argv, "i:o:b:e:")) != -1){
+        switch (c) {
+            case 'i':
+                in = optarg;
+                break;
+            case 'o':
+                out = optarg;
+                break;
+            case 'b':
+                bflag = 1;
+                bSamples = (int)strtol(optarg, (char **)NULL, 10);
+                break;
+            case 'e':
+                eflag = 1;
+                eSamples = (int)strtol(optarg, (char **)NULL, 10);
+                break;
+            case '?':
+                print_help();
+            default:
+                goto error;
+        }
+    }
+
+    check(!(bflag && eflag), "You cannot set both -b and -e");
+    check(bflag || eflag, "You need to specify the new length");
+    return;
+
+error:
+    abort();
+}
--- reader.c
+++ reader.c
@@ -1,3 +1,9 @@
+/*-
+ * Copyright (c) 2012 Meitian Huang
+ * All rights reserverd.
+ *
+ * Distributed under FreeBSD License.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,26 +15,23 @@
 
 extern char *in;
 extern char *out;
+extern int bflag;
+extern int eflag;
+extern int bSamples;
+extern int eSamples;
 
 unsigned int
-calcSample(FILE *f){
-    unsigned int result;
-
+calcSample(char *c){
     unsigned int subchunk2Size = 0;
-    fseek(f, 40, SEEK_SET);
-    fread(&subchunk2Size, 1, 4, f);
+    memcpy(&subchunk2Size, c + 40, 4);
 
     unsigned int bitsPerSample = 0;
-    fseek(f, 34, SEEK_SET);
-    fread(&bitsPerSample, 1, 2, f);
+    memcpy(&bitsPerSample, c + 34, 2);
 
     unsigned int numChannels = 0;
-    fseek(f, 22, SEEK_SET);
-    fread(&numChannels, 1, 2, f);
+    memcpy(&numChannels, c + 22, 2);
 
-    result = subchunk2Size * 8 / bitsPerSample / numChannels;
-
-    return result;
+    return subchunk2Size * 8 / bitsPerSample / numChannels;
 }
 
 
@@ -36,9 +39,8 @@ calcSample(FILE *f){
 main(int argc, char **argv)
 {
     parse(argc, argv);
-    if (in == NULL || out == NULL)
-    {
-        puts("You must say something...");
+    if (in == NULL || out == NULL){
+        fputs("You must say something...", stderr);
         abort();
     }
 
@@ -55,62 +57,73 @@ main(int argc, char **argv)
     fin = fopen(in, "rb");
     check(fin != NULL, "Cannot open the input file");
 
-    // Get the total size of the file. 
+    // Get the total size of the file.
     fseek(fin, 0, SEEK_END);
     origSize = ftell(fin);
     rewind(fin);
 
-    // Allocate enough memory to store the file. 
+    // Allocate enough memory to store the file.
     buffer = (char *) malloc(sizeof(char) * origSize);
     check_mem(buffer);
 
     // Read the input file.
     result = fread(buffer, 1, origSize, fin);
     check(result == origSize, "Cannot read the entire file");
+    fclose(fin);    // Content is already copied to memory.
+    // No need to use the input file.
 
     // Calculate the original number of samples.
-    origNumberSamples = calcSample(fin);
+    origNumberSamples = calcSample(buffer);
 
-    // Calculate the number of samples we want to get. 
-    newNumberSamples = origNumberSamples / 7 * 4;
+    if (bSamples > origNumberSamples || eSamples > origNumberSamples){
+        fputs("The specified number of samples is not legal.", stderr);
+        goto error;
+    }
+
+    int startAddr;
+
+    if (bflag) {
+        startAddr = 0;
+        newNumberSamples = bSamples;
+    } else if (eflag) {
+        startAddr = origNumberSamples - eSamples;
+        newNumberSamples = eSamples;
+    } else {
+        goto error;
+    }
+    // Calculate the number of samples we want to get.
 
     printf("Original Number of Samples:\t%d\n", origNumberSamples);
     printf("Current Number of Samples:\t%d\n", newNumberSamples);
 
-    // Retrieve the original subchunk2size. 
+    // Retrieve the original subchunk2size.
     unsigned int subchunk2Size = 0;
-    fseek(fin, 40, SEEK_SET);
-    result = fread(&subchunk2Size, 1, 4, fin);
-    check(result == 4, "Cannot read the file.");
+    memcpy(&subchunk2Size, buffer + 40, 4);
 
-    // New subchunk2size. 
-    newSubChunck2Size = subchunk2Size / origNumberSamples * newNumberSamples;
+    // New subchunk2size.
+    newSubChunck2Size 
+        = subchunk2Size / origNumberSamples * newNumberSamples;
 
-    // Calculate the adjustments. 
+    // Calculate the adjustments.
     unsigned int newChunkSize = 36 + newSubChunck2Size;
     unsigned int newLength = 8 + newChunkSize;
 
-    // Write the information to disk.
-    fout = fopen("test.wav", "wb");
+    // Adjust Chunksize.
+    memcpy(buffer + 4, &newChunkSize, 4);
+    /* Adjust Subchunk2Size */
+    memcpy(buffer + 40, &newSubChunck2Size, 4);
+
+    // Open the file to write
+    fout = fopen(out, "wb");
     check(fout != NULL, "Cannot open the output file.");
 
-    // Write without adjustment. 
-    result = fwrite(buffer, 1, newLength, fout);
+    // Write
+    result = fwrite(buffer + startAddr, 1, newLength, fout);
     check(result == newLength, "Cannot write to the file.");
 
-    // Adjust ChunkSize. 
-    fseek(fout, 4, SEEK_SET);
-    result = fwrite(&newChunkSize, 1, sizeof(unsigned int), fout);
-    check(result == sizeof(unsigned int), "Cannot write to the file.");
-
-    // Adjust Subchunk2Size.
-    fseek(fout, 40, SEEK_SET);
-    result = fwrite(&newSubChunck2Size, 1, sizeof(unsigned int), fout);
-    check(result == sizeof(unsigned int), "Cannot write to the file.");
-    
     free(buffer);
-    fclose(fin);
     fclose(fout);
+
     return 0;
 
 error:
